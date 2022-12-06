e716d9d8ae9563ccf299b47cc6d53eaae783696c
pythagoras58 <solomondanso58@gmail.com>
2022-12-06 23:32:05 +0000

    l

--- webAPI/Controllers/AuthorController.cs
+++ webAPI/Controllers/AuthorController.cs
@@ -8,13 +8,35 @@ namespace webAPI.Controllers
     public class AuthorController : ControllerBase
     {
        
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet]
         public IEnumerable<Author> Get()
         {
            // create an instance of db context and retrieve data from it
            using(var context = new BookStoresDbContext())
             {
                 return context.Authors.ToList();
+            }
+        }
+
+        [HttpGet("{id:int}", Name = "GetAuthorsById")]
+        public IEnumerable<Author> GetAuthorsById(int id)
+        {
+            using(var context = new BookStoresDbContext())
+            {
+                return context.Authors.Where(u=>u.AuthorId == id).ToList();
+            }
+        }
+
+        // post
+        [HttpPost]
+        public IEnumerable<Author> CreateAuthor(Author author)
+        {
+            using(var context = new BookStoresDbContext())
+            {
+                 context.Authors.Add(author);
+                 context.SaveChanges();
+                //return CreatedAtRoute(context,);
+                return (IEnumerable<Author>)Ok(context);
             }
         }
     }
